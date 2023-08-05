226a4004272e71d49f78825046668cab33959ca8
Maycol Perez <maycoldanielp@gmail.com>
2023-08-05 15:36:39 -0400

    fix: removed console log

--- src/components/ui/NavBar/Search.tsx
+++ src/components/ui/NavBar/Search.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import styles from './Search.module.css'
 
 const Search = () => {
-   const [inputFocus, setInputFocus] = useState(false)
-   
    const searchBarRef = useRef<HTMLDivElement>(null)
+   
+   const [inputFocus, setInputFocus] = useState(false)
    const [isMounted, setIsMounted] = useState(true)
-   console.log(isMounted)
+   
    const focusInputStyle = inputFocus ? 'ring-1 ring-blue-500' : 'ring-1 ring-neutral-700'
    
    const handleToggleInputFocus = () => {
@@ -36,21 +36,23 @@ const Search = () => {
    if ( isMounted ) {
       return (
          <div ref={ searchBarRef } className='flex rounded-3xl w-[38%]'>
-            <div className={`${styles.searchContainer} ${focusInputStyle} w-full`}>
+            <div className={`${styles.searchContainer} ${focusInputStyle} w-full `}>
                { inputFocus && (
-                  <AiOutlineSearch className='text-[20px] leading-none font-thin text-slate-300' />
+                  <div className='flex items-center rounded-l-3xl'>
+                     <AiOutlineSearch className='text-[20px] leading-none font-thin text-slate-300' />
+                  </div>
                )}
-               <input 
-                  onFocus={ handleToggleInputFocus }
-                  onBlur={ handleToggleInputFocus }
-                  type="text" 
-                  placeholder="Buscar"
-                  //            className='py-2 px-4 rounded-3xl bg-neutral-800 text-sm placeholder:text-gray-300 
-                  //    focus:outline-none focus:outline-1 focus:outline-blue-500'
-                  className='text-sm w-full bg-inherit placeholder:text-gray-300 text-gray-300 focus:outline-none'
-               />
+               <div className={`w-full py-0`}>
+                  <input 
+                     onFocus={ handleToggleInputFocus }
+                     onBlur={ handleToggleInputFocus }
+                     type="text" 
+                     placeholder="Buscar"
+                     className='text-sm w-full bg-inherit placeholder:text-gray-300 text-gray-300 focus:outline-none'
+                  />
+               </div>
             </div>
-            <span className={`${inputFocus ? 'ms-0.5' :''} h-full rounded-r-3xl flex items-center px-4 py-2 bg-[#222222] ring-1 ring-neutral-700`}>
+            <span className={`${inputFocus ? 'ms-0.5' :''} h-full rounded-r-3xl flex items-center px-4 py-2 bg-[#222222] ring-1 ring-neutral-700 cursor-pointer`}>
                <AiOutlineSearch className='text-[20px] leading-none font-thin text-slate-300' />
             </span>
          </div>
